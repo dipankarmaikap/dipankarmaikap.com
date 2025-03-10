371b7a2900a57f5b7b06f59828ab4bddc4228c9f
Dipankar Maikap <dipankarmaikap77@gmail.com>
2025-03-10 11:56:09 +0530

    JSON schema added for the archive pages

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,11 +1,12 @@
 export const perPage = 10;
 export const fburl = "https://www.facebook.com/dipankarmaikap77";
+export const siteUrl = "https://dipankarmaikap.com";
 export const authorName = "Dipankar Maikap";
 export const mySchema = {
   "@context": "https://schema.org",
   "@type": "Person",
-  name: "Dipankar Maikap",
-  url: "https://dipankarmaikap.com",
+  name: authorName,
+  url: siteUrl,
   description:
     "Web developer and tech blogger specializing in JavaScript, React, and web performance optimization.",
   image: "https://dipankarmaikap.com/profile.jpg",
@@ -38,3 +39,17 @@ export const mySchema = {
     "Node.js",
   ],
 };
+
+const { "@context": _, ...author } = mySchema;
+
+export const publisher = {
+  ...author,
+  logo: {
+    "@type": "ImageObject",
+    url: "https://dipankarmaikap.com/logo.png",
+    width: 600,
+    height: 60,
+  },
+};
+
+export { author };
